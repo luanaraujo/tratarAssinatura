cd215eba0372085cdf9cab1f42d97fa4df3c10f8
Luan Araujo <luangoncalves9@hotmail.com>
2023-06-26 16:27:38 -0300

    Comentários incluídos

--- imagem.py
+++ imagem.py
@@ -9,10 +9,12 @@ image1 = cv2.imread('img/alcicleia.jpeg')
 # Converte para escala de cinza
 img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
-# Limiar da imagem
+# Limiar da imagem e aplicação do filtro Gaussiano
 ret, limiar = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
 blur = cv2.GaussianBlur(limiar, (5, 5), 0)
 
+
+# Aplicação do filtro de mediana
 m, n = blur.shape
 img_new1 = np.zeros([m, n])
 for i in range(1, m-1):
@@ -29,6 +31,7 @@ for i in range(1, m-1):
         temp = sorted(temp)
         img_new1[i, j] = temp[4]
 img_new1 = img_new1.astype(np.uint8)
+
 # Mostra a imagem
 cv2.imshow('Limiar', img_new1)
 
